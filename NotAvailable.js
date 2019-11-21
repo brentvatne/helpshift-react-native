4be420af344b5429061e1e6044348e7a49f6fc33
Brent Vatne <brentvatne@gmail.com>
2019-11-21 15:42:04 -0800

    Noop when native module not available

---
+++ NotAvailable.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class Helpshift extends React.Component {
+  static eventEmitter = {
+    addListener = () => {},
+    removeListeners = () => {},
+    removeAllListeners = () => {},
+  };
+
+  static init = () => {};
+  static login = () => {};
+  static logout = () => {};
+  static showConversation = () => {};
+  static showFAQs = () => {};
+  static showConversationWithCIFs = () => {};
+  static showFAQsWithCIFs = () => {};
+  static requestUnreadMessagesCount = async () => 0;
+
+  componentDidMount() {
+    console.warn('Helpshift not available in this context');
+  }
+  render() {
+    return null;
+  }
+}
